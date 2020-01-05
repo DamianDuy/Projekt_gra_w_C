b90846de9ccba28f2fee7a3c9e36edceb4171b5b
Damian Duy <gamergray8@gmail.com>
2020-01-05 13:10:56 +0100

    Dodana piata mape

--- Damian_Duy_gra_logiczna.c
+++ Damian_Duy_gra_logiczna.c
@@ -18,6 +18,7 @@ void generate_map_first(int tab_size_vertical, int tab_size_horizontal, int tab_
 void generate_map_second(int tab_size_vertical, int tab_size_horizontal, int tab_map [] [tab_size_horizontal]); //Function that generates map
 void generate_map_third(int tab_size_vertical, int tab_size_horizontal, int tab_map [] [tab_size_horizontal]); //Function that generates map
 void generate_map_fourth(int tab_size_vertical, int tab_size_horizontal, int tab_map [] [tab_size_horizontal]); //Function that generates map
+void generate_map_fifth(int tab_size_vertical, int tab_size_horizontal, int tab_map [] [tab_size_horizontal]); //Function that generates map
 void show_map(int tab_size_vertical, int tab_size_horizontal, int tab_map [] [tab_size_horizontal]); //Function that prints map
 void user_input(int tab_size_vertical, int tab_size_horizontal, int tab_map [] [tab_size_horizontal], int num_of_stars, int * ptr_steps, int * ptr_if_the_same_map); //Function for taking user input and moving the character
 int stars_in_map_counter(int tab_size_vertical, int tab_size_horizontal, int tab_map [] [tab_size_horizontal]); //Function that counts elements to pick on the map
@@ -56,7 +57,7 @@ int main()
     //Looped menu
     do
     {
-        which_map = rand() % 4 + 1; //Getting a number of map that will be generated
+        which_map = rand() % 5 + 1; //Getting a number of map that will be generated
         if(*ptr_if_the_same_map != 0) 
         {
             which_map = temp_which_map; 
@@ -74,6 +75,7 @@ int main()
                 else if(which_map == 2) generate_map_second(tab_size_vertical, tab_size_horizontal, tab_map);
                 else if(which_map == 3) generate_map_third(tab_size_vertical, tab_size_horizontal, tab_map);
                 else if(which_map == 4) generate_map_fourth(tab_size_vertical, tab_size_horizontal, tab_map);
+                else if(which_map == 5) generate_map_fifth(tab_size_vertical, tab_size_horizontal, tab_map);
                 int num_of_stars = stars_in_map_counter(tab_size_vertical, tab_size_horizontal, tab_map);
                 system("clear");
                 printf("There are %d stars on the map.\n", stars_in_map_counter(tab_size_vertical, tab_size_horizontal,tab_map));
@@ -591,6 +593,72 @@ void generate_map_fourth(int tab_size_vertical, int tab_size_horizontal, int tab
     tab_map[1][16] = 300;
     tab_map[5][18] = 3;
     tab_map[14][18] = 3;
+}
+
+void generate_map_fifth(int tab_size_vertical, int tab_size_horizontal, int tab_map [] [tab_size_horizontal])
+{
+    //Filling an array with 4 where 4 represents ' '
+    for(int i = 0; i < tab_size_vertical; i++)
+        for(int j = 0; j < tab_size_horizontal; j++)
+        tab_map[i][j] = 4;
+
+    tab_map[1][1] = 0;    
+    
+    //Loops for drawing the edges of the map
+    for(int i = 0; i < tab_size_vertical; i++) tab_map[i][0] = 1;
+    for(int j = 0; j < tab_size_horizontal; j++)tab_map[0][j] = 2;
+    for(int i = 1; i < tab_size_vertical; i++) tab_map[i][tab_size_horizontal - 1] = 1;
+    for(int j = 0; j < tab_size_horizontal; j++)tab_map[tab_size_vertical - 1][j] = 2;
+
+    //Generating stars randomly
+    for(int i = 2; i < tab_size_vertical - 1; i++)
+    {
+        for(int j = 2; j < tab_size_horizontal - 1; j++)
+        {
+            if(tab_map[i][j] == 4)
+            {
+                tab_map[i][j] = rand() % 20 + 3;
+            }
+        }
+    }
+
+    ///Generating figures
+    tab_map[3][5] = 200;
+    tab_map[3][6] = 300;
+    tab_map[3][7] = 3;
+    tab_map[4][5] = 200;
+    tab_map[5][6] = 200;
+    tab_map[5][7] = 200;
+    tab_map[3][8] = 200;
+    tab_map[4][8] = 200;
+    tab_map[2][6] = 200;
+    tab_map[2][7] = 200;
+
+    tab_map[14][5] = 200;
+    tab_map[15][6] = 300;
+    tab_map[16][5] = 200;
+    tab_map[15][5] = 200;
+    tab_map[16][6] = 200;
+    tab_map[16][7] = 200;
+    tab_map[15][7] = 200;
+    tab_map[14][7] = 200;
+    tab_map[14][6] = 200;
+
+    tab_map[10][10] = 200;
+    tab_map[9][9] = 200;
+    tab_map[9][10] = 3;
+    tab_map[11][11] = 200;
+    tab_map[10][12] = 200;
+    tab_map[9][13] = 200;
+    tab_map[8][8] = 200;
+    tab_map[8][14] = 200;
+    tab_map[8][9] = 200;
+    tab_map[8][10] = 200;
+    tab_map[8][11] = 200;
+    tab_map[8][12] = 200;
+    tab_map[8][13] = 200;
+
+    tab_map[18][18] = 300;
 }
 
 void bonus()
